9db919248a038c6cb16039fb47a8e90166ab01e1
Ashik Pareeth <ashikpareeth007@gmail.com>
2026-05-20 23:44:42 +0530

    Remove unused import of MagicMock from test_trading_service.py

--- tests/test_trading_service.py
+++ tests/test_trading_service.py
@@ -1,6 +1,6 @@
 from decimal import Decimal
 from types import SimpleNamespace
-from unittest.mock import MagicMock, patch
+from unittest.mock import patch
 from uuid import uuid4
 
 import pandas as pd
